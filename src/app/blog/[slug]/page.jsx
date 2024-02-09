deaa9c06c01552d2ffcffa59e8c2f7286ebdf139
akashmukherjee009 <mukherjeeakash009@gmail.com>
2024-02-09 14:04:26 +0530

    End of Day4

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -6,23 +6,35 @@ import Image from 'next/image'
 const page = () => {
   return (
     <div className={styles.container}>
-      {/* <div className={styles.imgContainer}>
+      <div className={styles.imgContainer}>
         <Image src='/100.jpg' lt="About Image" fill className={styles.img}/>
-      </div> */}
+      </div>
       <div className={styles.textContainer}>
         <div className={styles.title}>
           Title
         </div>
         <div className={styles.detail}>
-        <Image src='/pot.jpg' lt="About Image" fill className={styles.avatar}/>
+        {/* <Image src='/pot.jpg' lt="About Image" fill className={styles.avatar}/> */}
         <div className={styles.detailText}>
           <span className={styles.detailTitle}>
             Author
           </span>
-          <span className={styles.detailValue}>
-            Date
+          <span className={styles.detailTitle}>
+            Published Date
           </span>
         </div>
+        <div className={styles.detailText}>
+          <span className={styles.detailValue}>
+            Akash009
+          </span>
+          <span className={styles.detailValue}>
+            09/02/2024
+          </span>
+        </div>
+        
+        </div>
+        <div className={styles.content}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor repellendus nihil facilis vero repudiandae. Delectus suscipit placeat similique provident. Odit est animi quo ad. Rerum enim labore corporis deserunt quos?
         </div>
       </div>
     </div>
